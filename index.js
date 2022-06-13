038ea0f1ba490e581a9603ce69f6e277416d58d7
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-13 10:49:58 +0300

    commit tree d37647c0e9a682d5ca27ed9e22ebbdf221a181e5

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+    let cats = ["Milo", "Otis", "Garfield"]
+    cats.sort("Milo", "Otis", "Garfield")
+
+
+    function destructivelyAppendCat(name){
+        return cats.push(name)
+
+    }
+
+    function destructivelyPrependCat(name){
+        return cats.unshift(name)
+    }
+
+    function destructivelyRemoveLastCat() {
+        return cats.pop()
+    }
+
+    function destructivelyRemoveFirstCat(){
+        return cats.shift()
+    }
+
+    function appendCat(name){
+        var newArray = cats.slice()
+        newArray.push(name)
+        return newArray
+    }
+
+    function prependCat(name){
+        var newArray = cats.slice()
+        newArray.unshift(name)
+        return newArray
+    }
+    
+    function removeLastCat(){
+        var newArray = cats.slice()
+        newArray.pop()
+        return newArray
+    }
+
+    function removeFirstCat(){
+        var newArray = cats.slice()
+        newArray.shift()
+        return newArray
+    }
